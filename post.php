4aa8b91cf564f74adc4e39fba05f9043d55fca4b
Parker <Kaninepete@gmail.com>
2012-12-25 15:10:51 -0500

    post.php correctly mines what is given

--- post.php
+++ post.php
@@ -1,8 +1,8 @@
 <?php
 $Owner = $_POST['Owner'];
-$Content = $_POST['Content']
+$Content = $_POST['Content'];
 
-echo "It worked!"
-echo $Owner
-echo $Content
+echo "It worked!";
+echo $Owner;
+echo $Content;
 ?>
