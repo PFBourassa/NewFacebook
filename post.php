719264a2138c4de0b3586d7f7ae40a4fe3c81a77
Parker <Kaninepete@gmail.com>
2012-12-25 15:33:11 -0500

    cleanup

--- post.php
+++ post.php
@@ -2,9 +2,9 @@
 $Owner = $_POST['Owner'];
 $Content = $_POST['Content'];
 
-#mysql_connect("localhost","kaninepe_neat","venasaur?") or die(mysql_error());
+#mysql_connect("localhost","kaninepe_neat","1admin") or die(mysql_error());
 
-echo "It worked!!";
+echo "It worked!";
 echo $Owner;
 echo $Content;
 ?>
