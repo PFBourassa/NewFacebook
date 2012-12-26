fb089423eb851a693e871c40954b1bd9665bb2d5
Parker <Kaninepete@gmail.com>
2012-12-26 13:42:16 -0500

    every post now has a timestamp

--- post.php
+++ post.php
@@ -1,11 +1,12 @@
 <?php
 $Owner = $_POST['Owner'];
 $Content = $_POST['Content'];
+$Time = date("r");
 
 mysql_connect("localhost", "kaninepe_neat", "venasuar?") or die(mysql_error());
 mysql_select_db("kaninepe_neat") or die(mysql_error());
 
-mysql_query("INSERT INTO Post (Owner, Content) VALUES('$Owner', '$Content') ") or die(mysql_error());
+mysql_query("INSERT INTO Post (Owner, Content, Time) VALUES('$Owner', '$Content', NOW() ) ") or die(mysql_error());
 
 #echo "It worked!";
 #echo $Owner;
