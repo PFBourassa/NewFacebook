19f16fcd3580aa81e4a58e0a0f0c2418b929e6bc
Parker <Kaninepete@gmail.com>
2013-01-02 11:02:04 -0500

    container for posts

--- view.php
+++ view.php
@@ -11,9 +11,11 @@ mysql_select_db("kaninepe_neat") or die(mysgl_error());
 $query = "SELECT * FROM Post ORDER BY time DESC";
 $result = mysql_query($query) or die(mysql_error());
 
+echo "<div class='feed'>";
 while($row = mysql_fetch_array($result)){
 	   echo "<div class='post'><p class='name'>".$row['Owner']."</p><p class = 'post_content'>".$row['Content']."</p></div>";
 	   echo "<br />";
 }
+echo "</div>";
 
 ?>
