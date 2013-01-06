c0bd69ced1be307ecfdfb2c5c3af0281209a5277
Parker <Kaninepete@gmail.com>
2013-01-06 14:08:44 -0500

    Logo with alpha

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-<h1>Neature Smash</h1>
+<h1><img src="Images/logoAlpha.png" alt="Neature Smash"></h1>
 <a href="post.html">post</a>
 <a href="view.php">view</a>
 <link rel="stylesheet" type="text/css" href="web.css" />
