b27644d96a391ca8ad5a28a5b72a6465ec09e9f9
Parker <Kaninepete@gmail.com>
2013-01-06 17:59:41 -0500

    changed link to logo

--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@
 <body>
 
 <div id="container">
-<h1><img src="Images/logoAlpha.png" alt="Neature Smash" id="home_logo"></h1>
+<h1><img src="Images/logo_green_better_kim.png" alt="Neature Smash" id="home_logo"></h1>
 <a href="post.html">post</a>
 <a href="view.php">view</a>
 
