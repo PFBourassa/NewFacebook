2eb821d06f92be03aa1e338c70a62d1c972d056f
hollyefig <hollyefig@gmail.com>
2013-01-06 16:39:48 -0500

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,7 +1,22 @@
 <html>
+
+<style>
+
+#container {
+width:960px;
+margin:0px auto;
+}
+
+</style>
+
 <body>
+
+<div id="container">
 <h1><img src="Images/logoAlpha.png" alt="Neature Smash"></h1>
 <a href="post.html">post</a>
 <a href="view.php">view</a>
 <link rel="stylesheet" type="text/css" href="web.css" />
 <br />
+</div>
+
+</body>
