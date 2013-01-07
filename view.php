983d0a43d7f11ff8c51abe1e59e97f4a16e4c67d
Parker <Kaninepete@gmail.com>
2013-01-07 16:10:05 -0500

    now displays timestamp

--- view.php
+++ view.php
@@ -13,7 +13,7 @@ $result = mysql_query($query) or die(mysql_error());
 
 echo "<div class='feed'>";
 while($row = mysql_fetch_array($result)){
-	   echo "<div class='post'><p class='name'>".$row['Owner']."</p><p class = 'post_content'>".$row['Content']."</p></div>";
+	   echo "<div class='post'><p class='name'>".$row['Owner']."</p><p class = 'post_content'>".$row['Content']."</p><span class='post_time'>".$row['Time']."</span></div>";
 	   echo "<br />";
 }
 echo "</div><!--feed-->";
