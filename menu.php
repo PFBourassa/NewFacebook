274007adeb536ce272d844e980e99fdc666fc798
Parker <Kaninepete@gmail.com>
2013-01-09 14:31:31 -0500

    header

--- menu.php
+++ menu.php
@@ -5,10 +5,11 @@
 </head>
 <body>
 
+<header>
 <div id="container">
 <h1><img src="Images/logo_green_better_kim.png" alt="Neature Smash" id="home_logo"></h1>
 <a href="post.html">post</a>
 <a href="view.php">view</a>
+</header>
 
-<br />
 </div>
