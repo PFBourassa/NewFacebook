161a3529eee0f1e92d8807452e865172c1664fce
Parker <Kaninepete@gmail.com>
2013-01-09 14:13:38 -0500

    dbconnect prints to console

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,6 @@
 <?php
 mysql_connect("localhost", "kaninepe_neat", "venasuar?") or die(mysql_error());
 mysql_select_db("kaninepe_neat") or die(mysql_error());
-echo "Connected to database"
+
+echo "<script type='text/javascript'>console.log('Connected to database');</script>"
 ?>
